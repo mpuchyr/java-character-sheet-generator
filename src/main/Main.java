8f5d27307cef2ee7c6e460b44cf5f0dfec107c23
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2022-03-01 14:06:08 -0600

    Add initial way for user to add name of character in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,18 @@ package src.main;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import src.main.models.characters.CoCCharacter;
 import src.main.models.characters.CoCCharacter.CharacterEra;
+import src.main.models.characters.GenericCharacter.Sex;
 
 
 public class Main implements ActionListener {
@@ -18,6 +21,7 @@ public class Main implements ActionListener {
     public static JButton generateCoCCharacterButton;
     public static JTextArea cocCharacterInfo;
     public static JScrollPane scroll;
+    public static JTextField nameField;
 
     public static void main(String[] args) {
 
@@ -35,6 +39,14 @@ public class Main implements ActionListener {
         generateCoCCharacterButton.addActionListener(new Main());
         panel.add(generateCoCCharacterButton);
 
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setBounds(10, 50, 50, 50);
+        panel.add(nameLabel);
+
+        nameField = new JTextField();
+        nameField.setBounds(75, 50, 50, 25);
+        panel.add(nameField);
+
         cocCharacterInfo = new JTextArea("");
         scroll = new JScrollPane(cocCharacterInfo);
         scroll.setBounds(10, 110, 500, 300);
@@ -47,8 +59,15 @@ public class Main implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, true);
-        cocCharacterInfo.setText(cocCharacter.toString());
+        String charName = nameField.getText();
+        if (charName.isBlank() || charName == null) {
+            CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, true);
+            cocCharacterInfo.setText(cocCharacter.toString());
+        } else {
+            CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, charName, Sex.MALE, true);
+            cocCharacterInfo.setText(cocCharacter.toString());
+        }
+
     }
 
 
