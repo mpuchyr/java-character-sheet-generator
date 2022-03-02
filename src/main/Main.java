de5261ffd511c32e4679883ac211910ffde7d2f1
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2022-03-02 16:22:31 -0600

    Add ability to choose gender of character created in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,15 @@
 package src.main;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.SwingPropertyChangeSupport;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,6 +25,9 @@ public class Main implements ActionListener {
     public static JTextArea cocCharacterInfo;
     public static JScrollPane scroll;
     public static JTextField nameField;
+    public static JRadioButton maleButton;
+    public static JRadioButton femaleButton;
+    public static Sex characterSex = Sex.MALE;
 
     public static void main(String[] args) {
 
@@ -40,16 +46,41 @@ public class Main implements ActionListener {
         panel.add(generateCoCCharacterButton);
 
         JLabel nameLabel = new JLabel("Name");
-        nameLabel.setBounds(10, 50, 50, 50);
+        nameLabel.setBounds(10, 50, 50, 25);
         panel.add(nameLabel);
 
         nameField = new JTextField();
-        nameField.setBounds(75, 50, 50, 25);
+        nameField.setBounds(50, 50, 200, 25);
         panel.add(nameField);
+
+        maleButton = new JRadioButton("Male");
+        maleButton.setSelected(true);
+        maleButton.setBounds(10, 75, 75, 25);
+        maleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                characterSex = Sex.MALE;
+            }
+        });
+
+        femaleButton = new JRadioButton("Female");
+        femaleButton.setBounds(10, 100, 75, 25);
+        femaleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                characterSex = Sex.FEMALE;
+            }
+        });
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(maleButton);
+        group.add(femaleButton);
+
+
+        panel.add(maleButton);
+        panel.add(femaleButton);
 
         cocCharacterInfo = new JTextArea("");
         scroll = new JScrollPane(cocCharacterInfo);
-        scroll.setBounds(10, 110, 500, 300);
+        scroll.setBounds(300, 50, 500, 300);
         panel.add(scroll);
 
 
@@ -60,11 +91,12 @@ public class Main implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String charName = nameField.getText();
+        
         if (charName.isBlank() || charName == null) {
             CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, true);
             cocCharacterInfo.setText(cocCharacter.toString());
         } else {
-            CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, charName, Sex.MALE, true);
+            CoCCharacter cocCharacter = new CoCCharacter(CharacterEra.NINETEENTWENTIES, charName, characterSex, true);
             cocCharacterInfo.setText(cocCharacter.toString());
         }
 
